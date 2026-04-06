d63300f96d3091389e7cb8f7e3a9e5a86856eb9c
Gowtham Kumar K. <115537077+GKK-Hub@users.noreply.github.com>
2026-04-06 23:21:07 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,31 @@
+import re
+from nltk.corpus import movie_reviews, stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+stop_words = stopwords.words('English')
+lemmatizer = WordNetLemmatizer()
+
+
+def preprocess(text):
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove punctuation and numbers
+    text = re.sub(r'[^a-z\s]', '', text)
+
+    # Tokenize
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    tokens = [token for token in tokens if token not in stop_words]
+
+    # Lemmatize: convert it back to root word
+    tokens  = [lemmatizer.lemmatize(token) for token in tokens]
+
+    # Join tokens back to string
+    return " ".join(tokens)
+
+
+if __name__ == "__main__":
+    print(preprocess("I'm not satisfied with this product... It say fresh which is why i purchased but I got the spoiled tapioca instead of fresh."))
