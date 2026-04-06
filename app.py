3dab885c4bed50eed4a3923d0aa51234b6ebce3c
Gowtham Kumar K. <115537077+GKK-Hub@users.noreply.github.com>
2026-04-07 00:24:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import joblib
 import numpy as np
 from preprocess import preprocess
@@ -31,5 +32,7 @@ def health():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     
